1ab1a16d94a8dbb36713107b5c18b682b2290773
Jordan Nguejip <yvesjordan06@gmail.com>
2025-03-02 19:51:40 -0500

    Added healthcheck

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HealthCheck;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::singleton('ping', HealthCheck::class);
